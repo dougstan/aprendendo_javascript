d846ee80c5ebe0c029fb5277127b581640888dc1
unknown <douglas.stanichesch@gmail.com>
2020-02-13 21:33:52 -0300

    Começando no git

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var nome = "Dunha";
+var n1 = 29;
+var n2 = 15;
+var frase = "Eu sou phoda!!!";
+//alert("Bem vindo, " + nome +", você tem " + idade + " Anos!");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+//console.log(frase);
+//console.log(frase.replace("sou", "não sou"));
+console.log(frase.toUpperCase());
