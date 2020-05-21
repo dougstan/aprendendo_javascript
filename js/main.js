17cb99c73c1d4685d5a0d55a59510ad1e7d76741
Douglas Stanichesch <60761186+dougstan@users.noreply.github.com>
2020-05-20 21:05:02 -0300

    Aula de array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/*
 var nome = "Dunha";
 var n1 = 29;
 var n2 = 15;
@@ -10,3 +10,25 @@ console.log(n1 * n2);
 //console.log(frase);
 //console.log(frase.replace("sou", "não sou"));
 console.log(frase.toUpperCase());
+*/
+
+/*
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva"); //por elemento no array
+//lista.pop(); //tira o último elemento
+//console.log(lista[1]);
+// dentro do console.log - lenght mosta a quantidade de elementos e reverse() inverte a ordem, de trás pra frente
+console.log(lista); 
+console.log(lista.toString()); // converte para string e mostra na tela, perde a referência de array
+console.log(lista.join(" / ")); // tipo um separador entre os elementos do array
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}; // "dicionário" - objeto com atributo
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]; // lista de "dicionários"
+console.log(frutas[1].nome);
+alert(frutas[1].cor);
